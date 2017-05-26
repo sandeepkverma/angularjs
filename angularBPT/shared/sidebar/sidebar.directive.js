55d9a3b4f2c70cb4c04fc8a508b4d8034f6ebeb8
mmi <mmi@com195>
2017-05-26 18:44:18 +0530

    new directive changes

--- angularBPT/shared/sidebar/sidebar.directive.js
+++ angularBPT/shared/sidebar/sidebar.directive.js
@@ -4,9 +4,37 @@
 	angular.module('shared.myWidget',[]).directive('testDirective',testDirective);
 
 	function testDirective(){
-		return {
-			template:"<h1>hello form directive</h1>"
+		
+		var directive = {
+			link:link,
+			templateUrl:'shared/sidebar/sidebarView.html',
+			restrict:'EA',
+			  scope: {
+        	    max: '='
+       		 },
+	        controller: ExampleController,
+	        controllerAs: 'vm',
+	        bindToController: true
+
+		}
+		return directive;
+		
+
+		function link(scope,element,attrs){
+
+			/* implementation is here */
 		}
 	}
+
+	function ExampleController() {
+    var vm = this;
+    vm.min = 3;
+    vm.$onInit = onInit;
+    
+    function onInit()  {
+        console.log('CTRL: vm.min = %s', vm.min);
+        console.log('CTRL: vm.max = %s', vm.max);
+    }
+}
 
 })();
